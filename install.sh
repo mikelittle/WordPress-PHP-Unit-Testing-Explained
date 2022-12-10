0d16697c933c3e4588491fdcead3d2de89913a64
Mike Little <mike+gh@zed1.com>
2022-12-10 01:39:12 +0000

    Changes to work wih newer MySQL and gitpod

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # Fail on any error.
 set -e
 
+cd bylines
+
 # Install Composer dependencies.
 echo -e "Step 1: Install Composer dependencies"
 composer install --no-suggest
@@ -10,11 +12,14 @@ composer install --no-suggest
 # Create the test database.
 echo -e "Step 2: Create the test database"
 echo -e " * Creating database 'byline_tests' (if it's not already there)"
-sudo mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS \`byline_tests\`"
+mysql -e "CREATE DATABASE IF NOT EXISTS \`byline_tests\`"
 echo -e " * Granting the wp user priviledges to the 'byline_tests' database"
-sudo mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON \`byline_tests\`.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -e "CREATE USER IF NOT EXISTS wp@localhost IDENTIFIED WITH mysql_native_password BY 'wp';"
+mysql -e "GRANT ALL PRIVILEGES ON \`byline_tests\`.* TO wp@localhost;"
 echo -e " * DB operations done."
 
 # Do a test run.
 echo -e "Step 3: Do a test run"
 composer test
+
+cd ..
